fac1c5e72e849b5ce7fa3589e372e7d558111b86
Pablo Campuzano Cuadrado <pbcampu@gmail.com>
2024-10-16 22:15:58 +0200

    3enraya acabado

--- 3enraya.js
+++ 3enraya.js
@@ -1,5 +1,3 @@
-
-
 function juego(){
     tablero = 
     [
@@ -17,18 +15,30 @@ function juego(){
             [posicionX, posicionY] = preguntaJugador(turno).split(",");
             console.log(posicionX, posicionY)
         } while(!validarPosicion(posicionX, posicionY))
+        
+
         if(turno == 1){
             console.log(posicionX)
-            tablero[posicionX-1][posicionY-1] == "X"
+            tablero[posicionX][posicionY] = "X"
             turno = 2
         } else {
-            tablero[posicionX+1][posicionY+1] == "O"
+            tablero[posicionX][posicionY] = "O"
             turno = 1
         }
-        imprimirtablero()
+        if(comprobarTablero() == "victoria"){
+            juego = false
+            alert("Jugador " + turno + " ha ganado\n" + imprimirtablero())
+        } else if(comprobarTablero() == "empate"){
+            juego = false
+            alert("Empate\n" + imprimirtablero())}
+
+        else {
+            alert(imprimirtablero())
+        }
     }
     function validarPosicion(posicionX, posicionY){
-        if(posicionX < 1 || posicionX > 3 || posicionY < 1 || posicionY > 3){
+        console.log(tablero[posicionX][posicionY])
+        if(posicionX < 0 || posicionX > 2 || posicionY < 0 || posicionY > 2){
             return false
         } else if((tablero[posicionX][posicionY] == "X" || tablero[posicionX][posicionY] == "Y")){
             return false
@@ -37,14 +47,74 @@ function juego(){
         }
     }
     function preguntaJugador(turno){
-        return prompt("Jugador " + turno + ", que casilla quieres marcar? (x,y)");
+        return prompt(imprimirtablero() + "\nJugador " + turno + ", que casilla quieres marcar? (x,y)");
     }
     
+    function comprobarTablero(){
+        tablero.forEach((fila) => {
+            fila.forEach((casilla) => {
+                if(casilla == "O" || casilla == "X"){
+                    return "empate"
+                }
+            })
+        })
+
+        for(let i = 0; i < 3; i++){
+            if((tablero[i][0] == tablero[i][1] && tablero[i][1] == tablero[i][2])){
+                if(tablero[i][0] == "#"){
+                    return "sigue"
+                }
+                return "victoria"
+            }
+            if(tablero[0][i] == tablero[1][i] && tablero[1][i] == tablero[2][i]){
+                if(tablero[0][i] == "#"){
+                    return "sigue"
+                }
+                return "victoria"
+            }
+            if(tablero[0][0] == tablero[1][1] && tablero[1][1] == tablero[2][2]){
+                if(tablero[0][0] == "#"){
+                    return "sigue"
+                }
+                return "victoria"
+            }
+            if(tablero[0][2] == tablero[1][1] && tablero[1][1] == tablero[2][0]){
+                if(tablero[0][2] == "#"){
+                    return "sigue"
+                }
+                return "victoria"
+            }
+            if(tablero[0][0] == tablero[0][1] && tablero[0][1] == tablero[0][2]){
+                if(tablero[0][0] == "#"){
+                    return "sigue"
+                }
+                return "victoria"
+            }
+            if(tablero[1][0] == tablero[1][1] && tablero[1][1] == tablero[1][2]){
+                if(tablero[1][0] == "#"){
+                    return "sigue"
+                }
+                return "victoria"
+            }
+            if(tablero[2][0] == tablero[2][1] && tablero[2][1] == tablero[2][2]){
+                if(tablero[2][0] == "#"){
+                    return "sigue"
+                }
+                return "victoria"
+            }
+        }
+        return "sigue"
+    }
+
     function imprimirtablero(){
         tablero_string = ""
         tablero.forEach(linea => {
-            tablero_string += linea + "\n"
+            linea.forEach(casilla => {
+                tablero_string += casilla + " "
+            });
+            tablero_string += "\n"
         });
+        return tablero_string
     }
     
 }
